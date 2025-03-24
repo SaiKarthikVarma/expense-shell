2630a736cd3410882c996d2b91293b2ff7cc2262
saikarthikvarmadatla <saikarthikvarma09@gmail.com>
2025-03-24 16:55:34 -0500

    expense backend

---
+++ backend.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then    
+        echo  -e "$2 is ... $R failed $N" | tee -a $LOG_FILE
+        exit 1
+    else 
+        echo -e "$2 is ... $G success $N" | tee -a $LOG_FILE
+    fi
+}
+
+check(){    
+    dnf list installed mysql
+    if [ $? -ne 0 ]
+    then
+        echo "mysql is not installed ...going to be installed"
+        dnf install mysql-server -y &>>$LOG_FILE
+        VALIDATE $? "Installing MySQL server"
+    else 
+        echo "mysql server is already installed"
+    fi    
+
+        
+
+}      
+
+
+echo "the script started executed at $(date)" | tee -a $LOG_FILE
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "user has no root access please proceed with the root access" | tee -a $LOG_FILE
+    exit 2
+fi
+
+dnf module disable nodejs -y
+VALIDATE $? "disable default node js"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "enable nodejs 20" 
+
+dnf install nodejs -y
+VALIDATE $? "installing node js"
+
+useradd expense
+VALIDATE $? "creating expense user"
